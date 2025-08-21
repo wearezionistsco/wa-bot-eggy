8e355ab563455f28a973cf3c2b7e8a6c6e1b78ac
wearezionistsco <wearezionists.co@gmail.com>
2025-08-22 05:04:49 +0700

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,9 @@
+module.exports = {
+    adminNumbers: ["6281234567890"], // Nomor admin (tanpa +)
+    sessionTimeouts: {
+        pesanan: 60 * 60 * 1000, // 1 jam
+        panggilan: 5 * 60 * 1000 // 5 menit
+    },
+    botName: "📲 BOT CUSTOMER SERVICE",
+    logFile: "logs/bot.log"
+};
